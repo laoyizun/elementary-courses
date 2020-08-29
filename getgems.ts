400f2af4160047f88ce648ecfb4ac7936d98b4a8
Lawrence <44716177+laoyizun@users.noreply.github.com>
2020-08-29 18:04:07 +0800

    更新 pxt.json, main.blocks, getgems.ts

--- getgems.ts
+++ getgems.ts
@@ -1,169 +1,130 @@
-// 在此处添加您的代码
 //% weight=100 color=#6699CC icon="\uf140" block="Getgems"
-//% groups='["Tiles", "Display", "Sprite"]'
-namespace  getgems{
-
+//% groups='["move"]'
+namespace getgems {
+enum turnDireciton{
+    anticlockwise = 0,
+    clockwise = 1
+    }
     //%block
     //%group="Sprite"
-    //%blockId=destoryAllOfKind block="destroy all of kind %kind=spritekind"
-    export function destroyAllSpriteOfKind(spriteKind:number) {
-        for (let targetSprite of sprites.allOfKind(spriteKind)) {
-            targetSprite.destroy()
-        }
+    //%blockId=turnRound block="turnRound %turnDirection"
+    export function turnRound (turnDirection: boolean) {
+    if (turnDirection) {
+        heroDirection += 1
+        heroDirection = heroDirection % 4
+    } else {
+        heroDirection += 3
+        heroDirection = heroDirection % 4
     }
-
-    let hpManagerSprites: Sprite[] = []
-    const CUBICBIRD_HELPER_BLOCKS_SPRITE_HP_DATA_KEY = 'CUBICBIRD_HELPER_BLOCKS_SPRITE_HP_DATA_KEY'
-
-    //%block
-    //% blockId=cubicbirdlocationOf block="location of %sprite=variables_get(mySprite)"
-    //% group="Tiles"
-    export function locationOf(sprite:Sprite) :tiles.Location {
-        return tiles.getTileLocation(tileColumnOfSprite(sprite), tileRowOfSprite(sprite))
+    if (heroDirection == 3) {
+        gemsHero.setImage(img`
+            . . . . . . f f f f f f . . . . 
+            . . . . f f e e e e f 2 f . . . 
+            . . . f f e e e e f 2 2 2 f . . 
+            . . . f e e e f f e e e e f . . 
+            . . . f f f f e e 2 2 2 2 e f . 
+            . . . f e 2 2 2 f f f f e 2 f . 
+            . . f f f f f f f e e e f f f . 
+            . . f f e 4 4 e b f 4 4 e e f . 
+            . . f e e 4 d 4 1 f d d e f . . 
+            . . . f e e e 4 d d d d f . . . 
+            . . . . f f e e 4 4 4 e f . . . 
+            . . . . . 4 d d e 2 2 2 f . . . 
+            . . . . . e d d e 2 2 2 f . . . 
+            . . . . . f e e f 4 5 5 f . . . 
+            . . . . . . f f f f f f . . . . 
+            . . . . . . . f f f . . . . . . 
+            `)
+    } else if (heroDirection == 0) {
+        gemsHero.setImage(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
+    } else if (heroDirection == 1) {
+        gemsHero.setImage(img`
+            . . . . f f f f f f . . . . . . 
+            . . . f 2 f e e e e f f . . . . 
+            . . f 2 2 2 f e e e e f f . . . 
+            . . f e e e e f f e e e f . . . 
+            . f e 2 2 2 2 e e f f f f . . . 
+            . f 2 e f f f f 2 2 2 e f . . . 
+            . f f f e e e f f f f f f f . . 
+            . f e e 4 4 f b e 4 4 e f f . . 
+            . . f e d d f 1 4 d 4 e e f . . 
+            . . . f d d d d 4 e e e f . . . 
+            . . . f e 4 4 4 e e f f . . . . 
+            . . . f 2 2 2 e d d 4 . . . . . 
+            . . . f 2 2 2 e d d e . . . . . 
+            . . . f 5 5 4 f e e f . . . . . 
+            . . . . f f f f f f . . . . . . 
+            . . . . . . f f f . . . . . . . 
+            `)
+    } else if (heroDirection == 2) {
+        gemsHero.setImage(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f e e e e f f . . . . 
+            . . . f e e e f f e e e f . . . 
+            . . f f f f f 2 2 f f f f f . . 
+            . . f f e 2 e 2 2 e 2 e f f . . 
+            . . f e 2 f 2 f f 2 f 2 e f . . 
+            . . f f f 2 2 e e 2 2 f f f . . 
+            . f f e f 2 f e e f 2 f e f f . 
+            . f e e f f e e e e f e e e f . 
+            . . f e e e e e e e e e e f . . 
+            . . . f e e e e e e e e f . . . 
+            . . e 4 f f f f f f f f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
     }
-
-    //%block
-    //% blockId=cubicbirdTileRowOfSprite block="tile row number of %sprite=variables_get(mySprite)"
-    //% group="Tiles"
-    export function tileRowOfSprite(sprite: Sprite): number {
-        return getPositionIndex(sprite.y)
-    }
-
-    //%block
-    //% blockId=cubicbirdTileColumnOfSprite block="tile column number of %sprite=variables_get(mySprite)"
-    //% group="Tiles"
-    export function tileColumnOfSprite(sprite: Sprite): number {
-        return getPositionIndex(sprite.x)
-    }
-
-   
-
-    //%block
-    //% group="Tiles"
-    export function getTileColumn(tile: tiles.Tile): number {
-        return getPositionIndex(tile.x)
-    }
-
-    //%block
-    //% blockId=cubicbirdtileisIndex block="is %tile=gamegettile of %index=colorindexpicker?"
-    //%group="Tiles"
-    export function tileIsIndex(tile: tiles.Tile, index: number): boolean {
-        return tile.tileSet == index
-    }
-
-    function getPositionIndex(x: number) {
-        return x >> game.currentScene().tileMap.scale
-    }
-
-
-    // let barWidth = NaN;
-
-    // //%block
-    // //% blockId=cubicbirddisplayHitPointBar block="show %x percent of hp || on %sprite=variables_get(mySprite)"
-    // //% group="Display"
-    // export function displayHitPointBar(x: number, sprite:Sprite=null) {
-    //     if (sprite != null) {
-    //         displaySpriteHitPointBarImpl(sprite, x)
-    //     } else {
-    //         if (x <= 0) {
-    //             barWidth = NaN;
-    //         } else {
-    //             barWidth = x / 5 * 6;
-    //         }
-            
-    //         game.onShade(function () {
-    //             if (barWidth) {
-    //                 screen.fillRect(20, 110, 120, 4, 1)
-    //                 screen.fillRect(20, 111, x / 5 * 6, 2, 3)
-    //             }
-    //         })
-    //     }
-        
-    // }
-
-    // function displaySpriteHitPointBarImpl(sprite: Sprite, x: number) {
-    //     if (!hpManagerSprites.find(element => element === sprite)) {
-    //         hpManagerSprites.push(sprite)
-    //         sprite.onDestroyed(() => {
-    //             hpManagerSprites.removeElement(sprite)
-    //         })
-    //     }
-    //     sprite.data[CUBICBIRD_HELPER_BLOCKS_SPRITE_HP_DATA_KEY] = x
-    // }
-
-
-    // //%block
-    // //% blockId=cubicbirdCircleSpirteAt block="circle %sprite=variables_get(mySprite) at (%x, %y) with radius %radius at speed %velocity"
-    // //% velocity.shadow=spriteSpeedPicker
-    // //% group="Sprite"
-    // export function circleSpriteAt(sprite:Sprite, x:number, y:number, r:number, velocity:number) {
-    //     sprite.x = x;
-    //     sprite.y = y - r;
-    //     sprite.data['currentRadius'] = 0;
-
-    //     let interval = Math.PI * r * 1000/ velocity / 180 
-        
-    //     game.onUpdateInterval(interval, ()=>{
-            
-    //         let currentRadius = sprite.data['currentRadius'];
-            
-    //         sprite.vx = Math.cos(currentRadius) * velocity;
-    //         sprite.vy = Math.sin(currentRadius) * velocity;
-
-    //         sprite.data['currentRadius'] = currentRadius + Math.PI / 180
-    //     })
-    // }
-
-
-
-    // //%block
-    // //% blockId=cubicbirdMoveSpriteTowardsOthersprite block="move %sprite=variables_get(mySprite) towards %otherSprite=variables_get(mySprite) || at velocity %veloctiy"
-    // //% group="Sprite"
-    // export function moveTowards(sprite:Sprite, otherSprite:Sprite, velocity:number=50) {
-    //     let distance = Math.sqrt( Math.pow(sprite.x - otherSprite.x, 2) + Math.pow(sprite.y - otherSprite.y, 2) )
-    //     sprite.vx = velocity * (otherSprite.x - sprite.x) / distance
-    //     sprite.vy = velocity * (otherSprite.y - sprite.y) / distance
-    // }
-
-
-    // //%block
-    // //%blockId=cubicbirdSpriteDistance block="distance of %sprite=variables_get(mySprite) and %otherSprite=variables_get(otherSprite)"
-    // //%group="Sprite"
-    // export function distance(sprite:Sprite, otherSprite:Sprite) :number {
-    //     return Math.sqrt(Math.pow(sprite.x - otherSprite.x, 2) + Math.pow(sprite.y - otherSprite.y, 2))
-    // }
-    
-    // //%block
-    // //%group="Sprite"
-    // //%blockId=cubicbirdNearestSpriteOfKind block="nearest sprite of %kind=spritekind to %sprite=variables_get(mySprite)"
-    // export function nearestSpriteOfKind(kind:number,sprite:Sprite) :Sprite {
-    //     let nearestDistance = 999999999;
-    //     let result :Sprite= null
-
-    //     for (let candidate of sprites.allOfKind(kind)) {
-    //         let currentDistance = distance(sprite, candidate)
-    //         if (currentDistance < nearestDistance) {
-    //             nearestDistance = currentDistance
-    //             result = candidate
-    //         }
-    //     }
-
-    //     return result;
-    // } 
-
-    // game.onShade(function () {
-    //     for (let hpManagedSprite of hpManagerSprites) {
-    //         let hpPercentage = hpManagedSprite.data[CUBICBIRD_HELPER_BLOCKS_SPRITE_HP_DATA_KEY]
-
-    //         if (hpPercentage > 0) {
-    //             let height = hpManagedSprite.image.height
-    //             let width = hpManagedSprite.image.width
-    //             let barWidth = (width - 2) * hpPercentage/100
-
-    //             screen.fillRect(hpManagedSprite.x - width / 2 + 1, hpManagedSprite.y - height / 2 - 2, width - 2, 1, 1)
-    //             screen.fillRect(hpManagedSprite.x - width / 2 + 1, hpManagedSprite.y - height / 2 - 2, barWidth, 1, 2)
-    //         }
-    //     }
-    // })
-
 }
+function stepForward () {
+    if (heroDirection == 3) {
+        heroRow += 1
+    } else if (heroDirection == 0) {
+        heroCol += 1
+    } else if (heroDirection == 1) {
+        heroRow += -1
+    } else if (heroDirection == 2) {
+        heroCol += -1
+    }
+    tiles.placeOnTile(gemsHero, tiles.getTileLocation(heroRow, heroCol))
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    turnRound(true)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    turnRound(false)
+})
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    stepForward()
+})
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+let gemsHero: Sprite = null
+let heroDirection = 0
+let heroCol = 0
+let heroRow = 0
+heroRow = 0
+heroCol = 0
+heroDirection = 0
+  
+}
+
