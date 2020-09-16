68ad21ab08ce92d9991c3a75b6678e704a09c6e9
Lawrence <44716177+laoyizun@users.noreply.github.com>
2020-09-16 10:19:34 +0800

    更新 pxt.json, main.blocks, main.ts, getgems.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    for (let index = 0; index < 3; index++) {
-        for (let index = 0; index < 6; index++) {
-            getgems.stepForward()
-        }
-        getgems.takeGems()
-        getgems.turn(turnDireciton.left)
-    }
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    getgems.turn(null)
 })
 getgems.initGame()
